a7694682c7d9ac9265ff2360c84df21429a30b41
Pablo Carrillo <pablo@calvium.com>
2014-12-17 12:42:47 +0000

    Update code to work on iOS 7 and with different rotations, use masonry for auto layout in code Also remove close button and add a gesture recogniser for dismiss the table

--- Pod/Classes/PDSearchHUD.h
+++ Pod/Classes/PDSearchHUD.h
@@ -15,26 +15,49 @@ typedef enum {
 
 @protocol PDSearchHUDDelegate <NSObject>
 
-//Called after User taps on the List and index of the item is returned
-- (void)didSelectRowAtIndex:(int)index;
+/**
+* @brief Called after User taps on the List and index of the item is returned
+* @param index the index of the cell taped
+*/
+- (void)didSelectRowAtIndex:(NSUInteger)index;
 
-//Called after User taps on the List and value is returned
+/**
+* @brief Called after User taps on the List and value is returned
+* @param item the string of the cell tapped
+*/
+
 - (void)didSelectItem:(NSString *)item;
 @end
 
 @interface PDSearchHUD : UIView <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 
-@property (nonatomic, strong) IBOutlet UITableView *tableView;
-@property (nonatomic, strong) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UIView *backgroundView;
 
-@property (nonatomic, assign) __unsafe_unretained id <PDSearchHUDDelegate>delegate;
+@property (nonatomic, weak) id <PDSearchHUDDelegate>delegate;
 @property (nonatomic, strong) NSArray *searchList;
 
 // Value that determines whether the HUD to dismiss after row selection
-@property (nonatomic, assign) BOOL dismissWhenRowSelected;
+@property (nonatomic) BOOL dismissWhenRowSelected;
 
-//Used to determine the Comparision type
-@property (nonatomic, assign) PDSearchType searchType;
+//Used to determine the Comparison type
+@property (nonatomic) PDSearchType searchType;
 
-- (id)initWithSearchList:(NSArray *)list andDelegate:(id)aDelegate;
+/**
+* @brief designated initializer
+*
+* @param list and array of NSStrings to be use on the table
+* @param aDelegate the PDSearchHUDDelegate to be use
+*
+* @return and instance of PDSearchHUD
+*
+*/
+- (instancetype)initWithSearchList:(NSArray *)list andDelegate:(id)aDelegate;
+
+/**
+* @brief call this method instead of addSubview in the superview to add the PDSearchHUD with the proper constraints to the superView
+* @param superview the view to hold the PDSearchHUD
+*/
+- (void)addToSuperView:(UIView *)superview;
 @end
--- Pod/Classes/PDSearchHUD.m
+++ Pod/Classes/PDSearchHUD.m
@@ -7,18 +7,23 @@
 //
 
 #import "PDSearchHUD.h"
+#import "View+MASAdditions.h"
+
+#define CELL_IDENTIFIER @"Cell"
 
 @interface PDSearchHUD ()
 
-@property (nonatomic, strong) NSArray *revisedList;
+@property(nonatomic, strong) NSArray *revisedList;
+@property(nonatomic, weak) UIView *visualEffectView;
+@property (nonatomic, weak) UIView *vibrancyView;
+@property(nonatomic, weak) UIView *viewToBlur;
 
 - (IBAction)tappedOnClose:(id)sender;
 @end
 
 @implementation PDSearchHUD
 
-- (id)initWithFrame:(CGRect)frame
-{
+- (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
@@ -26,9 +31,14 @@
     return self;
 }
 
-- (id)initWithSearchList:(NSArray *)list andDelegate:(id)aDelegate {
+- (instancetype)initWithSearchList:(NSArray *)list andDelegate:(id)aDelegate {
     CGRect frame = [[UIScreen mainScreen] bounds];
-    self = [[[NSBundle mainBundle] loadNibNamed:@"PDSearchHUD" owner:self options:nil] lastObject];
+    NSString *seachHUDBundlePath = [NSString stringWithFormat:@"%@/%@", [[NSBundle mainBundle] resourcePath],
+                                                              @"SearchHUD.bundle"];
+    NSBundle *searchHUDBundle = [NSBundle bundleWithPath:seachHUDBundlePath];
+    NSArray *views = [searchHUDBundle loadNibNamed:@"PDSearchHUD" owner:self options:nil];
+
+    self = [views lastObject];
     [self setFrame:frame];
     if (self) {
         // Initialization code
@@ -36,15 +46,77 @@
         _searchList = list;
         _revisedList = list;
         _searchType = PDSearchTypeContains;
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CELL_IDENTIFIER];
+        _dismissWhenRowSelected = YES;
     }
+    [self addEffects];
+    [self addGestureToBackground];
     return self;
+}
+
+- (void)addGestureToBackground {
+    UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                     action:@selector(tappedOnClose:)];
+    [self.backgroundView addGestureRecognizer:gestureRecognizer];
+}
+
+- (void)addEffects {
+    self.visualEffectView = nil;
+    if (NSClassFromString(@"UIVisualEffectView")) {
+        _backgroundView.backgroundColor = [UIColor clearColor];
+
+        UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+        UIVisualEffectView *visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+        [self insertSubview:visualEffectView belowSubview:self.backgroundView];
+        UIView *viewToBlur = [[UIView alloc] initWithFrame:[_visualEffectView frame]];
+        self.viewToBlur = viewToBlur;
+        viewToBlur.backgroundColor = [UIColor clearColor];
+        [visualEffectView.contentView addSubview:viewToBlur];
+        self.visualEffectView = visualEffectView;
+    }
+}
+
+- (void)addToSuperView:(UIView *)superview {
+//    self.alpha = 0.0f;
+    [superview addSubview:self];
+    [self addMasonryConstraints];
+//    self.alpha = 1.0f;
+}
+
+- (void)addMasonryConstraints {
+    NSInteger searchBarHeight = 44;
+    [self mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.bottom.right.left.equalTo(@0);
+    }];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self).insets(UIEdgeInsetsMake(20+searchBarHeight, 20, 20, 20));
+    }];
+    [self.searchBar mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.tableView);
+        make.bottom.equalTo(self.tableView.mas_top);
+        make.height.equalTo(@(searchBarHeight));
+    }];
+    [self.backgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self);
+    }];
+    if (self.visualEffectView) {
+        [self.visualEffectView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(self);
+        }];
+        [self.vibrancyView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(self);
+        }];
+        [self.viewToBlur mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(self);
+        }];
+    }
 }
 
 - (void)layoutSubviews {
     self.alpha = 0.0f;
     [UIView animateWithDuration:0.30 animations:^{
         self.alpha = 1.0f;
-    } completion:^(BOOL finished) {
+    }                completion:^(BOOL finished) {
 
     }];
     [super layoutSubviews];
@@ -65,38 +137,34 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *CellIdentifier = @"Cell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-    }
-    
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_IDENTIFIER forIndexPath:indexPath];
+
     //assign cell properties
-    cell.textLabel.text = [self.revisedList objectAtIndex:indexPath.row];
-    
+    cell.textLabel.text = (self.revisedList)[(NSUInteger) indexPath.row];
+
     return cell;
 }
 
 #pragma mark - Tableview Delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    NSString *selectedItem = [self.revisedList objectAtIndex:indexPath.row];
-    
+
+    NSString *selectedItem = (self.revisedList)[(NSUInteger) indexPath.row];
+
     if ([self.delegate respondsToSelector:@selector(didSelectRowAtIndex:)]) {
-        int index = [self.searchList indexOfObject:selectedItem];
-        [self.delegate didSelectRowAtIndex:index];
+        NSUInteger index = [self.searchList indexOfObject:selectedItem];
+        [self.delegate didSelectRowAtIndex:(NSUInteger) index];
     } else {
         NSLog(@"Sorry didSelectRowAtIndex: not implemented");
     }
-    
+
     if ([self.delegate respondsToSelector:@selector(didSelectItem:)]) {
         [self.delegate didSelectItem:selectedItem];
     } else {
         NSLog(@"Sorry didSelectItem: not implemented");
     }
-    
+
     if (_dismissWhenRowSelected) {
         [self tappedOnClose:nil];
     }
@@ -109,7 +177,7 @@
     if ([searchText length] == 0) {
         _revisedList = self.searchList;
     } else {
-        
+
         NSPredicate *predicate;
         switch (self.searchType) {
             case PDSearchTypeContains:
@@ -121,19 +189,25 @@
             default:
                 break;
         }
-        
+
         _revisedList = [self.searchList filteredArrayUsingPredicate:predicate];
     }
-    
+
     [self.tableView reloadData];
 }
 
 - (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
     [searchBar setShowsCancelButton:YES animated:YES];
+    [searchBar becomeFirstResponder];
+}
+
+- (BOOL)searchBarShouldBeginEditing:(UISearchBar *)searchBar {
+    return YES;
 }
 
 - (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
     _revisedList = self.searchList;
+    [searchBar resignFirstResponder];
     [self.tableView reloadData];
 }
 
@@ -147,7 +221,7 @@
 - (IBAction)tappedOnClose:(id)sender {
     [UIView animateWithDuration:0.30 animations:^{
         self.alpha = 0.1f;
-    } completion:^(BOOL finished) {
+    }                completion:^(BOOL finished) {
         [self removeFromSuperview];
     }];
 }
